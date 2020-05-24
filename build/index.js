86357f0d484c7da1fa77c44d72b7c3138db483f8
Dominique <dominique.lopez@digitregroup.com>
2020-05-24 16:59:06 +0200

    Added sort

--- build/index.js
+++ build/index.js
@@ -1,2 +1,8 @@
 "use strict";
-console.log('Hi there!');
+Object.defineProperty(exports, "__esModule", { value: true });
+var NumbersCollection_1 = require("./NumbersCollection");
+var Sorter_1 = require("./Sorter");
+var numbers = new NumbersCollection_1.NumbersCollection([1, 10, 5, 0, -1]);
+var sortNumbers = new Sorter_1.Sorter(numbers);
+sortNumbers.sort();
+sortNumbers.collection.displayData();
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log('Hi there!');
+import { NumbersCollection } from './NumbersCollection';
+import { Sorter } from './Sorter';
+
+const numbers = new NumbersCollection([1, 10, 5, 0, -1]);
+const sortNumbers = new Sorter(numbers);
+
+sortNumbers.sort();
+
+sortNumbers.collection.displayData();
