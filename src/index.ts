d379497a56361395222d8f93908671a28d9acb13
Dominique <dominique.lopez@digitregroup.com>
2020-05-27 11:53:28 +0200

    Added linkedList sort

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { CharactersCollection } from './CharactersCollection';
 import { NumbersCollection } from './NumbersCollection';
+import { LinkedList } from './LinkedList';
 import { Sorter } from './Sorter';
 
 const numbers = new NumbersCollection([1, 10, 5, 0, -1]);
@@ -11,3 +12,12 @@ const characters = new CharactersCollection('Hi there');
 const sorterCharacters = new Sorter(characters);
 sorterCharacters.sort();
 console.log(characters.data);
+
+const linkedList = new LinkedList();
+linkedList.add(232);
+linkedList.add(21);
+linkedList.add(-123);
+linkedList.add(123);
+const sorterLinkedList = new Sorter(linkedList);
+sorterLinkedList.sort();
+linkedList.print();
