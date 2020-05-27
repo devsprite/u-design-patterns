3090bcf510cfd69292360d6b129647befbdbc8de
Dominique <dominique.lopez@digitregroup.com>
2020-05-27 14:40:57 +0200

    Implement abstract classe Sorter

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,13 @@
 import { CharactersCollection } from './CharactersCollection';
 import { NumbersCollection } from './NumbersCollection';
 import { LinkedList } from './LinkedList';
-import { Sorter } from './Sorter';
 
 const numbers = new NumbersCollection([1, 10, 5, 0, -1]);
-const sorterNumbers = new Sorter(numbers);
-sorterNumbers.sort();
+numbers.sort();
 console.log(numbers.data);
 
 const characters = new CharactersCollection('Hi there');
-const sorterCharacters = new Sorter(characters);
-sorterCharacters.sort();
+characters.sort();
 console.log(characters.data);
 
 const linkedList = new LinkedList();
@@ -18,6 +15,5 @@ linkedList.add(232);
 linkedList.add(21);
 linkedList.add(-123);
 linkedList.add(123);
-const sorterLinkedList = new Sorter(linkedList);
-sorterLinkedList.sort();
+linkedList.sort();
 linkedList.print();
